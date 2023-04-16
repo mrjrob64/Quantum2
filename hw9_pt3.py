b0a36e86b1bd1bc48bb6dcda56ff69d1750722f7
mrjrob64 <mrjrob64@gmail.com>
2023-04-15 20:45:18 -0500

    hi

---
+++ hw9_pt3.py
@@ -0,0 +1,66 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+
+deltax = 0.001
+E = 0
+U0 = 20
+length = 5
+m = 1
+hbar = 1
+final_values_even = []
+final_values_odd = []
+allowed_energies = []
+
+def sw(x, X, B):
+    return (B+1)/2 + np.sign(.25 - (X-x)**2) * (B-1)/2
+
+def U(x):
+    return U0*sw(x,0,0)*sw(x,1.2,0)*sw(x,2.4,0)*sw(x,3.6,0)*sw(x,-1.2,0)*sw(x,-2.4,0)*sw(x,-3.6,0)
+
+
+x = []
+for i in range(int(length/deltax)):
+    x.append(i*deltax)
+
+#for i in range(0, len(y)):
+#    y[i] = U(i*deltax)
+
+deltaE = 0.01
+
+print("hi")
+
+for i in range(int(20/(deltaE))):
+    E  = E + deltaE
+    y = []
+    for i in range(len(x)):
+        y.append(0)
+    y[1] = 1
+    
+    for i in range(1, len(y)-1):
+        y[i+1] = 2*y[i] - y[i-1] + (deltax**2)*(2*m/(hbar**2))*(U(x[i]) - E)*y[i]
+    print("E :" + str(E) + " Value: " +str(y[len(y)-1]))
+    final_values_odd.append(y[len(y)-1])
+    if len(final_values_odd) > 1:
+        if final_values_odd[len(final_values_odd)-1]*final_values_odd[len(final_values_odd)-2] < 0:
+            allowed_energies.append(E)
+    
+E = 0
+for i in range(int(20/(deltaE))):
+    E  = E + deltaE
+    y = []
+    for i in range(len(x)):
+        y.append(0)
+    y[0] = 1
+    y[1] = 1
+    
+    for i in range(1, len(y)-1):
+        y[i+1] = 2*y[i] - y[i-1] + (deltax**2)*(2*m/(hbar**2))*(U(x[i]) - E)*y[i]
+    print("E :" + str(E) + " Value: " +str(y[len(y)-1]))
+    final_values_even.append(y[len(y)-1])
+    if len(final_values_even) > 2:
+        if final_values_even[len(final_values_even)-1]*final_values_even[len(final_values_even)-2] < 0:
+            allowed_energies.append(E)
+    
+
+print(allowed_energies)

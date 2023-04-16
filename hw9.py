ce19bdbe1b99ff55a9a559e765a8cc4e20097566
mrjrob64 <mrjrob64@gmail.com>
2023-04-15 20:16:32 -0500

    hi

--- hw9.py
+++ hw9.py
@@ -1,14 +1,15 @@
 import numpy as np
 
 import matplotlib.pyplot as plt
-#what is the differe
+
 deltax = 0.001
 E = 0
 U0 = 20
 length = 5
 m = 1
 hbar = 1
-final_values = []
+final_values_even = []
+final_values_odd = []
 allowed_energies = []
 
 def sw(x, X, B):
@@ -17,37 +18,50 @@ def sw(x, X, B):
 def U(x):
     return U0*sw(x,0,0)*sw(x,1,0)*sw(x,2,0)*sw(x,3,0)*sw(x,-1,0)*sw(x,-2,0)*sw(x,-3,0)
 
-#how do I turn a float into an int?
-
-
-y = []
-for i in range(int(length/deltax)):
-    y.append(0)
 
 x = []
-for i in range(len(y)):
+for i in range(int(length/deltax)):
     x.append(i*deltax)
-
-y[0] = 1
 
 #for i in range(0, len(y)):
 #    y[i] = U(i*deltax)
 
 deltaE = 0.01
 
-for i in range(int(20/(deltaE * 5))):
-    E = E + deltaE
+print("hi")
+
+for i in range(int(20/(deltaE))):
+    E  = E + deltaE
+    y = []
+    for i in range(len(x)):
+        y.append(0)
+    y[1] = 1
+    
     for i in range(1, len(y)-1):
         y[i+1] = 2*y[i] - y[i-1] + (deltax**2)*(2*m/(hbar**2))*(U(x[i]) - E)*y[i]
     print("E :" + str(E) + " Value: " +str(y[len(y)-1]))
-    final_values.append(y[len(y)-1])
-    if len(final_values) > 1:
-        if final_values[len(final_values)-1]*final_values[len(final_values)-2] < 0:
+    final_values_odd.append(y[len(y)-1])
+    if len(final_values_odd) > 1:
+        if final_values_odd[len(final_values_odd)-1]*final_values_odd[len(final_values_odd)-2] < 0:
             allowed_energies.append(E)
+    
+E = 0
+for i in range(int(20/(deltaE))):
+    E  = E + deltaE
     y = []
-    for i in range(int(length/deltax)):
+    for i in range(len(x)):
         y.append(0)
     y[0] = 1
+    y[1] = 1
+    
+    for i in range(1, len(y)-1):
+        y[i+1] = 2*y[i] - y[i-1] + (deltax**2)*(2*m/(hbar**2))*(U(x[i]) - E)*y[i]
+    print("E :" + str(E) + " Value: " +str(y[len(y)-1]))
+    final_values_even.append(y[len(y)-1])
+    if len(final_values_even) > 2:
+        if final_values_even[len(final_values_even)-1]*final_values_even[len(final_values_even)-2] < 0:
+            allowed_energies.append(E)
+    
 
 print(allowed_energies)
 
---
+++ hw9_pt2.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+L = 5
+print("HI")
+for n in range(1,15):
+    E = n**2 * np.pi**2/(2 *L**2)
+    println(E)

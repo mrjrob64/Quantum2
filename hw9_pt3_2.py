0d1a9739d9bf998fd7e3d951579bae5e50f7f0bc
mrjrob64 <mrjrob64@gmail.com>
2023-04-15 21:00:29 -0500

    bbababa

---
+++ hw9_pt3_2.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+
+n = []
+for i in range(1,15)
+    n.append(i)
+
+y = [2.22, 2.69, 3.28, 8.61, 9.87, 11.87, 13.84, 2.09, 2.42, 2.99, 3.49, 9.11, 10.81, 12.94]
+y = y.sort()
+
+plt.scatter(x, y)
+
+plt.show()
